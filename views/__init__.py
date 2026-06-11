a103f1bb2232effe537e4a0335a385fa8ab94ba5
HgManuel <manuelgomezhurtado1@gmail.com>
2026-06-10 22:56:49 -0500

    Create __init__.py to organize view imports

---
+++ views/__init__.py
@@ -0,0 +1,11 @@
+# views/__init__.py
+from .login_view import pantalla_login
+from .dashboard_view import pantalla_dashboard
+from .productos_view import pantalla_productos
+from .clientes_view import pantalla_clientes
+from .inventario_view import pantalla_inventario
+from .pos_view import pantalla_pos
+from .facturas_view import pantalla_facturas
+from .caja_view import pantalla_caja
+from .reportes_view import pantalla_reportes
+from .usuarios_view import pantalla_usuarios
